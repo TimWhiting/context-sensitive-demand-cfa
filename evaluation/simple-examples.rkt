24ee4366ddac140e921753514c33060adcc33d84
Tim Whiting <tim@whitings.org>
2023-12-20 12:34:14 -0700

    int example

--- evaluation/simple-examples.rkt
+++ evaluation/simple-examples.rkt
@@ -4,5 +4,6 @@
 (define example-id-0 `(app (λ (x) x) (λ (y) y)))
 (define example-0 `(app (λ (x) (app x x)) (λ (y) (app y y))))
 (define example-let-0 `(let ([x (λ (y) y)]) x))
+(define example-let-num `(let ([x (λ (y) y)]) (app x 1)))
 
-(define all-simple-examples (list example-id-0 example-0 example-let-0))
+(define all-simple-examples (list example-id-0 example-0 example-let-0 example-let-num))
